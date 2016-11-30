9d6c7877d069216f5038d6d03689f714d1bb24e2
Ai. Kirill <aist.k.work@gmail.com>
2016-12-01 01:49:30 +0200

    Added updating a User's information, PUT

--- app/server.js
+++ app/server.js
@@ -113,6 +113,46 @@ apiRouter.route('/users')
 	});
 
 // on routes that end in /users/user_id
+// ===========================================
+apiRouter.route('/users/:user_id')
+
+	//get the user with ID
+	// Accesed at GET http://localhist:8080/api/users/:user_id
+	.get(function(req, res){
+		User.findById(req.params.user_id, function(err, user){
+			if(err) res.send(err);
+
+			//return the user
+			res.json(user);
+		
+		})
+	})
+
+	//update user with ID
+	//accesed at PUT http://localhost:8080/api/users/:user_id
+
+	.put(function(req, res){
+		// use our user model to find the user we want
+
+		User.findById(req.params.user_id, function(err, user) {
+
+			if(err) res.send(err);
+
+			//update the users info inly if its new
+			if(req.body.name) user.name = req.body.name;
+			if(req.body.username) user.username = req.body.username;
+			if(req.body.password) user.password = req.body.password;
+
+			//save the user
+			user.save(function(err){
+				if(err) res.send(err);
+
+				// return a message
+				res.json({ message: 'User succesfully updated'});
+			});
+
+		});
+	})
 
 
 
